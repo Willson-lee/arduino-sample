6bc215b58ced665f63fb74e4fe21f810061154b0
William Stan <williamstanlee2206@gmail.com>
2024-05-13 22:50:01 +0530

    first commit

--- connecting_to_internet/connecting_to_internet.ino
+++ connecting_to_internet/connecting_to_internet.ino
@@ -1,30 +1,215 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Arduino_JSON.h>
 
-const char *ssid =  "Your wifi Network name";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "Network password";
+const char* ssid = "OPPO F19 Pro+";
+const char* password = "Wills@22";
 
-WiFiClient client;
- 
-void setup() 
-{
-       Serial.begin(9600);
-       delay(10);
-               
-       Serial.println("Connecting to ");
-       Serial.println(ssid); 
- 
-       WiFi.begin(ssid, pass); 
-       while (WiFi.status() != WL_CONNECTED) 
-          {
-            delay(500);
-            Serial.print(".");
+//Your Domain name with URL path or IP address with path
+String serverName = "http://192.168.0.145:3000";
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+
+
+int ledPin = 13;  // GPIO13---D7 of NodeMCU
+WiFiServer server(80);
+int RelayPin = 1;
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+	pinMode(RelayPin, OUTPUT);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+
+void loop() {
+
+  digitalWrite(RelayPin, LOW);
+	delay(3000);
+	
+	// Let's turn off the relay...
+	digitalWrite(RelayPin, HIGH);
+	delay(3000);
+
+  // Send an HTTP POST request depending on timerDelay
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
+      WiFiClient client;
+      HTTPClient http;
+
+      String serverPath = serverName + "/led";
+
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverPath.c_str());
+
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+
+        JSONVar myObject = JSON.parse(payload);
+
+        // JSON.typeof(jsonVar) can be used to get the type of the var
+        if (JSON.typeof(myObject) == "undefined") {
+          Serial.println("Parsing input failed!");
+          return;
+        }
+
+        Serial.print("JSON object = ");
+        Serial.println(myObject);
+
+        JSONVar keys = myObject.keys();
+
+
+        for (int i = 0; i < keys.length(); i++) {
+          JSONVar value = myObject[keys[i]];
+          Serial.print(keys[i]);
+          Serial.print(" = ");
+          Serial.println(value);
+
+          String vale = JSON.stringify(keys[i]);
+          Serial.print(" vals = ");
+          Serial.print(vale);
+          String vv = "\"data\"";
+          Serial.print(vv);
+
+          if (vale.equalsIgnoreCase(vv)) {
+            JSONVar dataKeys = value.keys();
+            Serial.println("yes");
+
+            for (int j = 0; j < dataKeys.length(); j++) {
+              JSONVar value2 = value[dataKeys[j]];
+              Serial.print(dataKeys[j]);
+              Serial.print(" = ");
+              Serial.println(value2);
+              if (JSON.stringify(dataKeys[j]) == "\"isLightStatus\"") {
+                if (value2) {
+                  Serial.print(" on = ");
+                  digitalWrite(ledPin, HIGH);
+                  value = HIGH;
+                } else {
+                  Serial.print(" off = ");
+                  digitalWrite(ledPin, LOW);
+                  value = LOW;
+                }
+              }
+            }
+
+          } else {
+            Serial.println("no");
           }
-      Serial.println("");
-      Serial.println("WiFi connected"); 
-}
- 
-void loop() 
-{      
-  
-}
+          // sensorReadingsArr[i] = double(value);
+        }
 
+
+      } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+  // Check if a client has connected
+  WiFiClient client = server.available();
+
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while (!client.available()) {
+    delay(1);
+  }
+
+
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  int value = LOW;
+  if (request.indexOf("/MY_LED=ON") != -1) {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/MY_LED=OFF") != -1) {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+
+  // Set ledPin according to the request
+  //digitalWrite(ledPin, value);
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");  //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.print("Led is now: ");
+
+  if (value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Off </button></a><br />");
+  client.println("</html>");
+
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
